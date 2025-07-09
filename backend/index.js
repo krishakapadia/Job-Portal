7261a95c35651a2c14799d923707b380417c1ecc
Krisha Kapadia <krisha.kapadia@somaiya.edu>
2025-07-10 02:03:30 +0530

    models & user controller with user APIs

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+
 dotenv.config({})
 const app = express();
 
@@ -18,6 +20,11 @@ app.use(cors(corsOptions));
 
 
 const PORT= process.env.PORT || 3000;
+
+//APIs
+app.use("/api/v1/user", userRoute);
+
+
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
