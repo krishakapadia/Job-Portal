e624c011bae94a29a5b0240a7393f96a363f5884
Krisha Kapadia <krisha.kapadia@somaiya.edu>
2025-07-10 02:50:25 +0530

    company controller

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const companySchema= new mongoose.Schema({
 name:{
     type:String,
-    required:true
+    required:true,
+    unique:true
 },
 description:{
      type:String,
@@ -27,3 +28,5 @@ userId:{
 
 },
 {timestamps:true});
+
+export const Company = mongoose.model("Company", companySchema);
