a76d6e74241837de1d29e5c200fd99f5f8a1043f
Krisha Kapadia <krisha.kapadia@somaiya.edu>
2025-07-11 16:01:41 +0530

    Vite + React + tailwind installation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-red-500 flex items-center justify-center">
+      <h1 className="text-red-500">Let's build Job Portal</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ui/shared/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white'>
+
+
+    </div>
+  )
+}
+
+export default Navbar
